e4fb755881240ea23063a0c0c80e2e3f98d2d0da
clearsky <38609798+clearsky@users.noreply.github.com>
2022-08-17 21:51:01 +0800

    Update LibImageBase.h

--- libimage/LibImageBase.h
+++ libimage/LibImageBase.h
@@ -3,11 +3,11 @@
 #include <vector>
 #include <string>
 #include <map>
-#include "LibImage_types.h"
-#include "WImage.h"
-#include "color.h"
-#include "dict.h"
-#include "ThreadPool.h"
+#include "./libimage/LibImage_types.h"
+#include "./libimage/WImage.h"
+#include "./libimage/color.h"
+#include "./libimage/dict.h"
+#include "..\libimage\ThreadPool.h"
 class LibImageBase
 {
 public:
@@ -18,6 +18,7 @@ public:
 	long UseDict(int idx);
 	long Ocr(Dict& dict, double sim, std::wstring& ret_str);
 	long OcrOne(Dict& dict, double sim, std::wstring& ret_str);
+	long OcrOneWithPos(Dict& dict, double sim, ocr_ret_with_pos& ret_str);
 	long OcrEx(Dict& dict, double sim, std::map<point_t, std::wstring>& ret);
 	long FindStrFast(const std::wstring& str, double sim);
 	long FindStrFastEx(const std::wstring& str,  double sim, std::map<point_t, std::wstring>& ret);
@@ -26,6 +27,8 @@ public:
 	long LoadPic(const std::wstring& name, WImage* pImg);
 	long FindPic(std::vector<WImage*>& pics, color_t dfcolor, double sim, point_desc_t&ret);
 	long FindPicEx(std::vector<WImage*>& pics, color_t dfcolor, double sim, vpoint_desc_t& vpd);
+	long FindColor(std::vector<color_df_t>& colors, point_t& ret, int sim_color);
+	long CmpColor(color_t color, std::vector<color_df_t>& colors, double sim);
 protected:
 	void record_sum(const WImageBin& gray);
 	int region_sum(int x1, int y1, int x2, int y2);
@@ -33,26 +36,40 @@ protected:
 	void _bin_ocr(const T& words, double sim, std::map<point_t, std::wstring>& ps, bool find_one = false);
 	void _find_pic(WImage* pic, const color_t& dfcolor,  long&x, long&y);
 	void _find_pic_ex(WImage* pic, const color_t& dfcolor, std::vector<point_t>& ret);
-protected:
+public:
 	void bgr2binary(std::vector<color_df_t>& colors);
 	void bin_ocr(const Dict& dict, double sim, std::map<point_t, std::wstring>& ps, bool one =false);
 	void bin_find_str(const std::wstring& strs, double sim, std::map<point_t, std::wstring>& ps, bool find_one = false);
-protected:
+public:
 	int VUINT642Colordfs(const std::vector<UINT64>& dfcolor_pre, std::vector<color_df_t>& colors);
 	void UINT322Color(const UINT32 v, color_t& color);
 	bool word_part_match(const rect_t& rc, int max_error, const uint8_t* data);
+	void SetOffset(int x1, int y1) {
+		_x1 = x1;
+		_y1 = y1;
+	}
 protected:
 	const static int _max_dict = 20;
+#if LIBIMAGE_UNIT_DEBUG
+public:
+#else:
 protected:
+#endif
 	Dict _dicts[_max_dict];
 	std::map<std::wstring, std::vector<int>>  _words_cache[_max_dict];
 	std::map<std::wstring, WImage*> _img_cache;
 	int _cur_dic_idx;
 	ThreadPool m_threadPool;
+#if LIBIMAGE_UNIT_DEBUG
+public:
+#else:
 protected:
-	WImage _src; // Ô´Í¼Ïñ
-	WImageBin _record; // ¼ÇÂ¼Í¼
-	WImageBin _binary; // ¶şÖµ»¯Í¼Ïñ
-	WImage _sum; // ÏñËØÃÜ¶ÈÍ¼
+#endif
+	WImage _src; // æºå›¾åƒ
+	WImageBin _record; // è®°å½•å›¾
+	WImageBin _binary; // äºŒå€¼åŒ–å›¾åƒ
+	WImage _sum; // åƒç´ å¯†åº¦å›¾
+	int _x1 = 0;
+	int _y1 = 0;
 };
 
